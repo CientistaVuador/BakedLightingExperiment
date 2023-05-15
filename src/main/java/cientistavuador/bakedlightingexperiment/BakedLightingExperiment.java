41f18f2cd92d4c34e9b207331a4233a39b3cbb95
CientistaVuador <52110280+CientistaVuador@users.noreply.github.com>
2023-05-15 18:29:23 -0300

    First Commit

---
+++ src/main/java/cientistavuador/bakedlightingexperiment/BakedLightingExperiment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cientistavuador.bakedlightingexperiment;
+
+/**
+ *
+ * @author Cien
+ */
+public class BakedLightingExperiment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
